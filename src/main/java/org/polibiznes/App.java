33b75cbf5d67d39686e8aa7e5d6805c4becab7f8
Damian Osiński <osinskidamian03@gmail.com>
2024-12-24 23:22:20 +0100

    Basic window settings

--- src/main/java/org/polibiznes/App.java
+++ src/main/java/org/polibiznes/App.java
@@ -1,13 +1,17 @@
 package org.polibiznes;
 
+import javax.swing.*;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Polibiznes");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
